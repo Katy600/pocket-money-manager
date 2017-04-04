d2d9871f1d6a9ce35d1bfd3c9320e9578e70b9f7
Katy McCann <katy.alice.mccann@gmail.com>
2017-04-04 15:41:48 +0100

    Create parent model and succesful migration

--- pocket_money_manager/app/controllers/parents_controller.rb
+++ pocket_money_manager/app/controllers/parents_controller.rb
@@ -4,7 +4,7 @@ class ParentsController < ApplicationController
   end
 
   def new
-    
+    @parent = Parent.new
   end
 
 end
--- pocket_money_manager/spec/parents_spec.rb
+++ pocket_money_manager/spec/parents_spec.rb
@@ -6,5 +6,15 @@ feature 'User can sign in' do
       visit '/parents'
       expect(page).to have_link('Sign up')
     end
+
+    it 'takes user to a registration page' do
+      visit '/parents'
+      click_link 'Sign up'
+      expect(page).to have_content('First name')
+      expect(page).to have_content('Last name')
+      expect(page).to have_content('Email')
+      expect(page).to have_content('Password')
+      expect(page).to have_button('Submit')
+    end
   end
 end
